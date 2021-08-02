7cfc6dfc102f79a07eb72cb66cf034a9b1ba6632
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2021-08-02 12:38:11 +0800

    change to overflow-auto

--- client/src/views/MasterView.tsx
+++ client/src/views/MasterView.tsx
@@ -10,7 +10,7 @@ export const MasterView = ({ children }: Props): ReactElement<HTMLDivElement> =>
   return (
     <div className="flex flex-col h-full w-full">
       <Header />
-      <div className="mt-4 ml-8 mr-8 mb-4 h-full overflow-hidden">{children}</div>
+      <div className="mt-4 ml-8 mr-8 mb-4 h-full overflow-auto">{children}</div>
     </div>
   );
 };
