2cc12c26bc67b43928766fccfcdf91434ee9969f
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2021-08-02 08:52:52 +0800

    load thunk

--- client/src/redux/configureStore.ts
+++ client/src/redux/configureStore.ts
@@ -1,13 +1,14 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { connectRoutes } from 'redux-first-router';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './rootReducer';
 import { routesMap } from './routing/routesMap';
 
 const { reducer, middleware, enhancer } = connectRoutes(routesMap);
 const reducers = combineReducers({ ...rootReducer, location: reducer });
-const middlewares = applyMiddleware(middleware);
+const middlewares = applyMiddleware(thunk, middleware);
 const enhancers = composeWithDevTools(enhancer, middlewares);
 export const store = createStore(reducers, enhancers);
 
