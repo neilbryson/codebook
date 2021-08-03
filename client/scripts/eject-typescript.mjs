ed3267f824623c42ab0962b5aa7633ad6b8e64ea
Neil Bryson Cargamento <neilbryson@users.noreply.github.com>
2021-08-03 12:16:45 +0800

    provide type checking even on ejected typescript

--- client/scripts/eject-typescript.mjs
+++ client/scripts/eject-typescript.mjs
@@ -3,7 +3,7 @@
 // https://github.com/storybookjs/addon-kit/blob/main/scripts/eject-typescript.mjs
 
 // Emit TypeScript declarations
-await $`tsc --declaration --emitDeclarationOnly --isolatedModules false --outfile codebook.js`;
+await $`tsc --project tsconfig.eject.json --declaration --emitDeclarationOnly --outdir ejectTypes`;
 
 // Copy TS files and delete src
 await $`cp -r ./src ./srcTS`;
@@ -11,7 +11,7 @@ await $`rm -rf ./src`;
 await $`mkdir ./src`;
 
 // Convert TS code to JS
-await $`babel --no-babelrc --presets @babel/preset-typescript --out-file-extension \".jsx\" ./srcTS -d ./src --extensions \".js,.jsx,.ts,.tsx\" --ignore "./srcTS/typings.d.ts"`;
+await $`babel --no-babelrc --presets @babel/preset-typescript --out-file-extension \".jsx\" ./srcTS -d ./src --extensions \".js,.jsx,.ts,.tsx\"`;
 
 // Format the newly created .js files
 await $`prettier --no-config --no-editorconfig --single-quote --trailingComma es5 --semi true --write ./src`;
@@ -19,8 +19,10 @@ await $`prettier --no-config --no-editorconfig --single-quote --trailingComma es
 // Copy css
 await $`cp -r ./srcTS/styles ./src/styles`
 
-// Copy types
-await $`mv codebook.d.ts ./src/codebook.d.ts`;
+// Add reference to the TypeScript types, so there will still be auto-completion
+// and type checking even in JavaScript files.
+await $`touch src/index.d.ts`;
+await $`echo '/// <reference path="../ejectTypes/index.d.ts">' > ./src/index.d.ts`;
 
 // Clean up
 await $`rm -rf ./srcTS`;
